d444b40145c538e413781194ffbabb9f08ab1ded
Choco Bibi <108186672+choco-bibi@users.noreply.github.com>
2022-06-26 20:27:36 +0100

    Create helloyou.c

---
+++ c1/helloyou.c
@@ -0,0 +1,16 @@
+/* helloyou.c*/
+/* Hello with test string. */
+
+////// 3.2 Part 2 //////
+
+#include <stdio.h>
+
+#define SUCCESS 0
+
+char name [] = "human";
+
+int main()
+	{
+		printf (" Hello %s!\n", name) ;
+		return SUCCESS;
+	}
