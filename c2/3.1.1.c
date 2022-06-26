a002adf3c3feb0635296eb30f7ca3fbf46284055
Choco Bibi <108186672+choco-bibi@users.noreply.github.com>
2022-06-26 21:02:41 +0100

    Update and rename 3.1 to 3.1.1.c

---
+++ c2/3.1.1.c
@@ -0,0 +1,23 @@
+/* 3.1.1.c*/
+/* function  */
+
+////// 3.1 Part 1  of 2 //////
+
+#include <stdio.h>
+
+#define SUCCESS 0
+
+int main()
+	{
+    unsigned long int x = 0;
+    while(1){
+      printf("%d\n", x);
+      x++;
+    }
+
+		return SUCCESS;
+	}
+
+// Enters an endless loop
+// Ctrl + C to stop the program
+
