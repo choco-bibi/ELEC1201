ecce45f06b313e78883146f7d711d93d157253a0
Choco Bibi <108186672+choco-bibi@users.noreply.github.com>
2022-06-26 20:49:05 +0100

    Create mark.c

---
+++ c1/mark.c
@@ -0,0 +1,26 @@
+/* mark.c*/
+/* __LINE__ constant  */
+
+////// 4 Optional Additional Work //////
+
+#include <stdio.h>
+
+#define SUCCESS 0
+#define DEBUG
+#ifdef DEBUG
+	#define MARK printf("%d\n", __LINE__);
+#else
+	#define MARK
+#endif
+
+int main()
+	{
+    MARK
+    
+
+
+		MARK
+		return SUCCESS;
+	}
+
+// __LINE__ is a number representing the line number your code is currently on
