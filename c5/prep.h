d43adf97fb003cff0f264d54cd36e4f93aed0459
Choco Bibi <108186672+choco-bibi@users.noreply.github.com>
2022-07-17 19:39:54 +0100

    Create prep.h

---
+++ c5/prep.h
@@ -0,0 +1,15 @@
+// weh why is there so much skeleton code
+// just going to enter code I wrote
+
+// in matrix.h
+typedef struct {
+	int rows;
+	int cols;
+	double **element;
+} Matrix;
+
+//in vector.h
+typedef struct {
+	int length;
+	double *element;
+} Vector;
