77ba1c6642378b8b6d5a9396703ba895c9e593d3
Choco Bibi <108186672+choco-bibi@users.noreply.github.com>
2022-07-18 10:02:29 +0100

    Create analyse.c

---
+++ c5/analyse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "circuit.h"
+
+int main(int argc, char *argv[])
+{
+	Circuit c;
+	
+	if (argc == 2)
+	{
+		c=createCircuitFromFile(argv[1]);
+		analyseCircuit(c);
+		destroyCircuit(c);
+	} 
+	else 
+		printf("Syntax: %s <filename>\n", argv[0]);
+
+	return EXIT_SUCCESS;
+}
